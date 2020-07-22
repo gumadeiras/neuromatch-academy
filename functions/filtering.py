e1598cc45fd4e31425bcc31cc66afe0bdde97dfd
Gustavo Santana <gumadeiras@gmail.com>
2020-07-22 10:23:01 -0400

    added filtering functions to find regions with recordings

---
+++ functions/filtering.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def recordings_with_region(data, region):
+    # returns recording index that has a region and number of recorded neurons
+    recorded_with_region = []
+    for idx, dat in enumerate(data):
+        regions, count = np.unique(dat['brain_area'], return_counts=True)
+        if region in regions:
+            pos = np.where(dat['brain_area'] == region)[0]
+            recorded_with_region.append([idx, pos.shape[0]])
+
+    return np.vstack(recorded_with_region)
+
+def regions_recorded(data):
+    # returns all regions recorded
+    recorded_regions = np.array([])
+    for idx, dat in enumerate(data):
+        # recorded brain regions
+        regions, count = np.unique(dat['brain_area'], return_counts=True)
+        recorded_regions = np.hstack([regions, recorded_regions])
+        
+    recorded_regions, animal_count = np.unique(recorded_regions, return_counts=True)
+    return list(zip(recorded_regions, animal_count))
+
+def regions_recorded_per_animal(data):
+    # returns regions recorded per animal
+    recorded_regions = []
+    for idx, dat in enumerate(data):
+        # recorded brain regions
+        regions, count = np.unique(dat['brain_area'], return_counts=True)
+        recorded_regions.append([idx, list(zip(regions, count))])
+
+    return np.vstack(recorded_regions)
