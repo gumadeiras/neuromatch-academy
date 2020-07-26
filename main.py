573bec15de5fd11cb608a73175c4513eab284400
heitorrapela <hrm@cin.ufpe.br>
2020-07-26 17:33:01 -0300

    Add psth plot to main

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ print(f"number of recordings: {alldat.shape}")
 
 regions_recorded(alldat)
 
-is_top = False
+is_top = True
 is_save = True
 sigmas = [0.1, 1.0 , 2.0]
 cr_top10 = ["ZI", "APN", "MRN", "SCm", "PO", "LD", "SNr", "SSp", "MOp", "MOs"]
 cr_others = ["SCs", "MG", "VPM", "VPL", "MD","CP", "PL", "ACA", "RSP", "VISam"]
 n_bins = 50
-root_folder = join('./average_activity_neurons_all_trials', ('top10' if is_top else 'others'))
+root_folder = join('./psth', ('top10' if is_top else 'others'))
 
-
+'''
 if is_top:
     regions = cr_top10
 else:
@@ -45,3 +45,22 @@ for region in regions:
         for sigma in sigmas:
             save_path = join(root_folder, region) + '_average_activity_neurons_all_trials_' + str(sigma) + '.png'
             plot_average_activity_neurons_all_trials(neurons_spks, region, sigma=sigma, save_path=save_path, save=is_save)
+
+'''
+
+if is_top:
+    regions = cr_top10
+else:
+    regions = cr_others
+
+for region in regions:
+    for dat in alldat:
+        neurons = dat['brain_area'] == region
+        neurons_spks = dat['spks'][neurons]
+        neurons_count = neurons_spks.shape[0]
+        total_neurons_count = dat['spks'].shape[0]
+        trials_count = dat['spks'].shape[1]
+        save_path = join(root_folder, region) + '_psth.png'
+        if neurons_count > 1:
+#             plot_average_activity_neurons_all_trials(neurons_spks, region, sigma=2)
+            psth(neurons_spks, region, timebin_size=5, save_path=save_path, save=is_save)
